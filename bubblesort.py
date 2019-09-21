1d64f685d769bb9b67426d59aec05de1dd4d5142
Bradley M. Small <bradley_small@hotmail.com>
2019-09-21 18:23:16 -0400

    cleaning with pylint3 & pycodestyle

--- bubblesort.py
+++ bubblesort.py
@@ -1,26 +1,49 @@
+#!/usr/bin/python3
+"""
+Program to demonstrate the bubble sort in python
+"""
 import random
+
 
 MAX = 10
 
+
 def main():
     """
-    main driver. creates the random list, then prints it, then calls 
+    main driver. creates the random list, then prints it, then calls
     the sort function, outputs the sorted list.
     """
+    # This is an already sorted list
+    # random_list = range(MAX)
+
+    # This is a reverse sorted list
+    # random_list = range(MAX-1,-1, -1)
+
+    # This is a randomly unsorted list
     random_list = random.sample(range(MAX), MAX)
 
-    print random_list
+    print(random_list)
     passes = list_bubblesort(random_list)
-    print random_list
-    print "Completed in %d pass%s." % (passes, "es" if 1 != passes else "") 
+    print(random_list)
+    print("Completed in %d pass%s." % (passes, "es" if passes != 1 else ""))
 
-def list_bubblesort(theList):
+
+def swap(the_list, src_idx, dst_idx):
+    """
+    using the indexes swaps two values in a list
+    """
+    tmp = the_list[dst_idx]
+    the_list[dst_idx] = the_list[src_idx]
+    the_list[src_idx] = tmp
+
+
+def list_bubblesort(the_list):
     """
     classic bubble sort
     tracking how many passes
 
     starting from the first item in the list
-    walk the list until it can be swapped with a lower value or switch 
+    walk the list until it can be swapped with a lower value or switch
     to the larger value and continue down the list
 
     loop back to the first item in the list
@@ -31,30 +54,22 @@ def list_bubblesort(theList):
     pass_no = 0
     done = False
     while not done:
-        idx = 0
         swapped = False
-        value = theList[idx]
-        for current in range(MAX):
-            if value > theList[current]:
-                tmp = theList[idx]
-                theList[idx] = theList[current]
-                theList[current] = tmp
-
-                idx = current
-                value = theList[idx]
-                
+        for next_one in range(1, len(the_list)):
+            current = next_one - 1
+            value = the_list[current]
+            if value > the_list[next_one]:
+                swap(the_list, current, next_one)
                 swapped = True
-            else:
-                idx = current
-                value = theList[idx]
 
         if not swapped:
             done = True
         else:
             pass_no += 1
-            print theList, pass_no
+            print(the_list, pass_no)
+
     return pass_no
 
-if __name__=="__main__":
-    main()
 
+if __name__ == "__main__":
+    main()
